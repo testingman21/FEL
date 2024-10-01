b02cb39a4f039f8a4f391332d4d52b34d4bae120
testingman21 <elcrunchycrispy@gmail.com>
2024-09-30 15:01:57 -1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function clicked(){
+    document.getElementById("test").innerText = "Clicked"
+}
+function next(){
+    location.href = "page2.html"
+}
+function future(){
+    location.href = "future.html"
+}
+function about(){
+    location.href = "index.html"
+}
+function concept(){
+    location.href = "concept.html"
+}
+function hist(){
+    location.href = "history.html"
+}
+function present(){
+    location.href = "present.html"
+}
